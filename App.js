f426c97eac87fe61ba305f4e60a19abc9ae0b67a
Antony Delavi <oantonygdelavi@gmail.com>
2025-04-13 12:13:48 -0300

    AGORA FAZ VENDA PORRAAAA

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB conectado'))
   .catch(err => console.error(err));
