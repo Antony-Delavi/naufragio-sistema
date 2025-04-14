441b296e9a4fc9363b65d4fe57d1804869a477a1
Antony Delavi <oantonygdelavi@gmail.com>
2025-04-14 16:43:25 -0300

    oi

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const vendasRoutes = require('./routes/vendas')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
