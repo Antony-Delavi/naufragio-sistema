3c24c7810f7d3b8e9400f75887e1ebd5578a0f1a
AntonyDelavi <oantonygdelavi@gmail.com>
2025-04-16 23:47:11 -0300

    Configurações de segurança e atualização das informações de produtos

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -24,7 +24,7 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  const { username, password } = xss(req.body);
+  const { username, password } = req.body;
 
   try {
     const user = await User.findOne({ username });
