471d6d46b1a6c803bffe35ee58b972a483c05b5a
AntonyDelavi <oantonygdelavi@gmail.com>
2025-04-17 20:41:10 -0300

    aaa

--- routes/backup.js
+++ routes/backup.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = router.express();
+const router = express.Router();
 const authLogin = require('../middleware/authLogin')
 const fazerBackup = require('../backup/googleDriveBackup')
 
